ceb4f0c7aea934a5eb78e6f9c4b74d548c8b0605
darshan <darshan@tomnext.co>
2025-07-29 14:58:59 +0530

    Committed changes for 2nd level claim security guarde mechanisem #151

---
+++ src/Benraz.Infrastructure.Authorization/ClaimValidators/ClaimFilterType.cs
@@ -0,0 +1,18 @@
+﻿namespace Benraz.Infrastructure.Authorization.ClaimValidators
+{
+    /// <summary>
+    /// Claim filter type.
+    /// </summary>
+    public enum ClaimFilterType
+    {
+        /// <summary>
+        /// Exact (matches claims that are an exact (case-insensitive)).
+        /// </summary>
+        Exact = 1,
+
+        /// <summary>
+        /// Include (matches claims that contain the specified value).
+        /// </summary>
+        Include = 2
+    }
+}
---
+++ src/Benraz.Infrastructure.Authorization/ClaimValidators/ClaimMatchType.cs
@@ -0,0 +1,18 @@
+﻿namespace Benraz.Infrastructure.Authorization.ClaimValidators
+{
+    /// <summary>
+    /// Claim match type.
+    /// </summary>
+    public enum ClaimMatchType
+    {
+        /// <summary>
+        /// All (matches all specified claims).
+        /// </summary>
+        All = 1,
+
+        /// <summary>
+        /// At least one (requires at least one matching claim).
+        /// </summary>
+        AtLeastOne = 2
+    }
+}
